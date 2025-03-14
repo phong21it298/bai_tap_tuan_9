c68e8f1795c2331fdb353a1fb3c5877d2ef923b5
nymph <phongtt.21it@vku.udn.vn>
2025-03-14 19:35:00 +0700

    Bai tap tuan 9

--- Bai_tap_tuan_8/app/src/main/java/com/example/bai_tap_tuan_8/Course.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bai_tap_tuan_8
-
-data class Course(
-
-    val id: Int,
-    val name: String,
-    val description: String
-)
--- Bai_tap_tuan_8/app/src/main/java/com/example/bai_tap_tuan_8/lab_2/Product.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bai_tap_tuan_8.lab_2
-
-data class Product(
-
-    val id_product: Int,
-    val name_product: String,
-    val desc_product: String,
-    val price_product: Double
-)
